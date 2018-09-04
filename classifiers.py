ff6488c69e95bf41edcdc6c67c0d1c6deb5f28c5
marco-marinho <marcoobom@gmail.com>
2018-09-04 10:26:20 -0300

    Updated medição tempo execução e precisão

--- classifiers.py
+++ classifiers.py
@@ -12,6 +12,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neural_network import MLPClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.decomposition import PCA
+from sklearn.cluster import DBSCAN
 from neupy import algorithms
 from pyearth import Earth
 import time
@@ -32,8 +33,13 @@ def classifica(X_train,X_test,Y_train,Y_test):
     score = accuracy_score(Y_test, Y_Hat)
     loss = log_loss(Y_test, Y_Hat_prob)
 
+    end = time.time()
+
     print('SVM: ')
     print('Precisão: '+str(round(score,4))+' Logloss: ' +str(round(loss,4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
 
     classifier = linear_model.LinearRegression()
     classifier.fit(X_train, Y_train)
@@ -41,8 +47,13 @@ def classifica(X_train,X_test,Y_train,Y_test):
     Y_Hat = np.around(Y_Hat)
     score = accuracy_score(Y_test, Y_Hat)
 
+    end = time.time()
+
     print('Regressao Linear: ')
     print('Precisão: '+str(round(score,4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
 
     classifier = DecisionTreeClassifier( max_depth = 4 )
 
@@ -53,8 +64,13 @@ def classifica(X_train,X_test,Y_train,Y_test):
     score = accuracy_score(Y_test, Y_Hat)
     loss = log_loss(Y_test, Y_Hat_prob)
 
+    end = time.time()
+
     print('Árvore de Decisão: ')
     print('Precisão: '+str(round(score,4))+' Logloss: ' +str(round(loss,4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
 
     classifier = GaussianNB()
 
@@ -65,8 +81,13 @@ def classifica(X_train,X_test,Y_train,Y_test):
     score = accuracy_score(Y_test, Y_Hat)
     loss = log_loss(Y_test, Y_Hat_prob)
 
+    end = time.time()
+
     print('Bayes: ')
     print('Precisão: '+str(round(score,4))+' Logloss: ' +str(round(loss,4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
 
     classifier = MLPClassifier(hidden_layer_sizes=(25,25,25))
 
@@ -77,8 +98,13 @@ def classifica(X_train,X_test,Y_train,Y_test):
     score = accuracy_score(Y_test, Y_Hat)
     loss = log_loss(Y_test, Y_Hat_prob)
 
+    end = time.time()
+
     print('Rede Neural: ')
     print('Precisão: '+str(round(score,4))+' Logloss: ' +str(round(loss,4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
 
     classifier = neighbors.KNeighborsClassifier()
 
@@ -89,16 +115,26 @@ def classifica(X_train,X_test,Y_train,Y_test):
     score = accuracy_score(Y_test, Y_Hat)
     loss = log_loss(Y_test, Y_Hat_prob)
 
+    end = time.time()
+
     print('KNN: ')
     print('Precisão: '+str(round(score,4))+' Logloss: ' +str(round(loss,4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
 
     classifier = cluster.KMeans(n_clusters=2)
     classifier.fit_transform(X_train)
     Y_Hat = classifier.predict(X_test)
     score = accuracy_score(Y_test, Y_Hat)
 
+    end = time.time()
+
     print('Cluster: ')
     print('Precisão: '+str(round(score,4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
 
     classifier = linear_model.LogisticRegression()
     classifier.fit(X_train, Y_train)
@@ -108,8 +144,13 @@ def classifica(X_train,X_test,Y_train,Y_test):
     score = accuracy_score(Y_test, Y_Hat)
     loss = log_loss(Y_test, Y_Hat_prob)
 
+    end = time.time()
+
     print('Regressão Logística: ')
     print('Precisão: '+str(round(score,4))+' Logloss: ' +str(round(loss,4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
 
     classifier = LinearDiscriminantAnalysis()
     classifier.fit(X_train, Y_train)
@@ -119,8 +160,13 @@ def classifica(X_train,X_test,Y_train,Y_test):
     score = accuracy_score(Y_test, Y_Hat)
     loss = log_loss(Y_test, Y_Hat_prob)
 
+    end = time.time()
+
     print('LDA: ')
     print('Precisão: '+str(round(score,4))+' Logloss: ' +str(round(loss,4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
 
     classifier = algorithms.GRNN(std=1, verbose=False)
     classifier.fit(X_train, Y_train)
@@ -128,8 +174,13 @@ def classifica(X_train,X_test,Y_train,Y_test):
     Y_Hat = np.around(Y_Hat)
     score = accuracy_score(Y_test, Y_Hat)
 
+    end = time.time()
+
     print('GRNN: ')
     print('Precisão: '+str(round(score,4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
 
     classifier = Earth()
     classifier.fit(X_train, Y_train)
@@ -137,9 +188,23 @@ def classifica(X_train,X_test,Y_train,Y_test):
     Y_Hat = np.around(Y_Hat)
     score = accuracy_score(Y_test, Y_Hat)
 
+    end = time.time()
+
     print('MARS: ')
     print('Precisão: '+str(round(score,4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
+
+    classifier = DBSCAN()
+    classifier.fit(X_train)
+    Y_Hat = classifier.fit_predict(X_test)
+    score = accuracy_score(Y_test, Y_Hat)
+
     end = time.time()
+
+    print('DBSCAN: ')
+    print('Precisão: '+str(round(score,4)))
     print('Tempo de execução:' + str(end - start))
 
 
@@ -149,19 +214,24 @@ def classificaPCA(X_train,X_test,Y_train,Y_test,components):
     pca = PCA(n_components=components)
     X_train = pca.fit_transform(X_train)
     X_test = pca.fit_transform(X_test)
-    
+
     start = time.time()
     classifier = SVC(probability=True)
 
     classifier.fit(X_train, Y_train)
     Y_Hat = classifier.predict(X_test)
     Y_Hat_prob = classifier.predict_proba(X_test)
-    
+
     score = accuracy_score(Y_test, Y_Hat)
     loss = log_loss(Y_test, Y_Hat_prob)
 
+    end = time.time()
+
     print('SVM: ')
-    print('Precisão: '+str(round(score,4))+' Logloss: ' +str(round(loss,4)))
+    print('Precisão: ' + str(round(score, 4)) + ' Logloss: ' + str(round(loss, 4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
 
     classifier = linear_model.LinearRegression()
     classifier.fit(X_train, Y_train)
@@ -169,64 +239,94 @@ def classificaPCA(X_train,X_test,Y_train,Y_test,components):
     Y_Hat = np.around(Y_Hat)
     score = accuracy_score(Y_test, Y_Hat)
 
+    end = time.time()
+
     print('Regressao Linear: ')
-    print('Precisão: '+str(round(score,4)))
+    print('Precisão: ' + str(round(score, 4)))
+    print('Tempo de execução:' + str(end - start))
 
-    classifier = DecisionTreeClassifier( max_depth = 4 )
+    start = time.time()
 
-    classifier.fit( X_train, Y_train )
+    classifier = DecisionTreeClassifier(max_depth=4)
+
+    classifier.fit(X_train, Y_train)
     Y_Hat = classifier.predict(X_test)
     Y_Hat_prob = classifier.predict_proba(X_test)
 
     score = accuracy_score(Y_test, Y_Hat)
     loss = log_loss(Y_test, Y_Hat_prob)
 
+    end = time.time()
+
     print('Árvore de Decisão: ')
-    print('Precisão: '+str(round(score,4))+' Logloss: ' +str(round(loss,4)))
+    print('Precisão: ' + str(round(score, 4)) + ' Logloss: ' + str(round(loss, 4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
 
     classifier = GaussianNB()
 
-    classifier.fit( X_train, Y_train )
+    classifier.fit(X_train, Y_train)
     Y_Hat = classifier.predict(X_test)
     Y_Hat_prob = classifier.predict_proba(X_test)
 
     score = accuracy_score(Y_test, Y_Hat)
     loss = log_loss(Y_test, Y_Hat_prob)
+
+    end = time.time()
 
     print('Bayes: ')
-    print('Precisão: '+str(round(score,4))+' Logloss: ' +str(round(loss,4)))
+    print('Precisão: ' + str(round(score, 4)) + ' Logloss: ' + str(round(loss, 4)))
+    print('Tempo de execução:' + str(end - start))
 
-    classifier = MLPClassifier(hidden_layer_sizes=(25,25,25))
+    start = time.time()
 
-    classifier.fit( X_train, Y_train )
+    classifier = MLPClassifier(hidden_layer_sizes=(25, 25, 25))
+
+    classifier.fit(X_train, Y_train)
     Y_Hat = classifier.predict(X_test)
     Y_Hat_prob = classifier.predict_proba(X_test)
 
     score = accuracy_score(Y_test, Y_Hat)
     loss = log_loss(Y_test, Y_Hat_prob)
 
+    end = time.time()
+
     print('Rede Neural: ')
-    print('Precisão: '+str(round(score,4))+' Logloss: ' +str(round(loss,4)))
+    print('Precisão: ' + str(round(score, 4)) + ' Logloss: ' + str(round(loss, 4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
 
     classifier = neighbors.KNeighborsClassifier()
 
-    classifier.fit( X_train, Y_train )
+    classifier.fit(X_train, Y_train)
     Y_Hat = classifier.predict(X_test)
     Y_Hat_prob = classifier.predict_proba(X_test)
 
     score = accuracy_score(Y_test, Y_Hat)
     loss = log_loss(Y_test, Y_Hat_prob)
 
+    end = time.time()
+
     print('KNN: ')
-    print('Precisão: '+str(round(score,4))+' Logloss: ' +str(round(loss,4)))
+    print('Precisão: ' + str(round(score, 4)) + ' Logloss: ' + str(round(loss, 4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
 
     classifier = cluster.KMeans(n_clusters=2)
     classifier.fit_transform(X_train)
     Y_Hat = classifier.predict(X_test)
     score = accuracy_score(Y_test, Y_Hat)
 
+    end = time.time()
+
     print('Cluster: ')
-    print('Precisão: '+str(round(score,4)))
+    print('Precisão: ' + str(round(score, 4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
 
     classifier = linear_model.LogisticRegression()
     classifier.fit(X_train, Y_train)
@@ -236,8 +336,13 @@ def classificaPCA(X_train,X_test,Y_train,Y_test,components):
     score = accuracy_score(Y_test, Y_Hat)
     loss = log_loss(Y_test, Y_Hat_prob)
 
+    end = time.time()
+
     print('Regressão Logística: ')
-    print('Precisão: '+str(round(score,4))+' Logloss: ' +str(round(loss,4)))
+    print('Precisão: ' + str(round(score, 4)) + ' Logloss: ' + str(round(loss, 4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
 
     classifier = LinearDiscriminantAnalysis()
     classifier.fit(X_train, Y_train)
@@ -247,8 +352,13 @@ def classificaPCA(X_train,X_test,Y_train,Y_test,components):
     score = accuracy_score(Y_test, Y_Hat)
     loss = log_loss(Y_test, Y_Hat_prob)
 
+    end = time.time()
+
     print('LDA: ')
-    print('Precisão: '+str(round(score,4))+' Logloss: ' +str(round(loss,4)))
+    print('Precisão: ' + str(round(score, 4)) + ' Logloss: ' + str(round(loss, 4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
 
     classifier = algorithms.GRNN(std=1, verbose=False)
     classifier.fit(X_train, Y_train)
@@ -256,8 +366,13 @@ def classificaPCA(X_train,X_test,Y_train,Y_test,components):
     Y_Hat = np.around(Y_Hat)
     score = accuracy_score(Y_test, Y_Hat)
 
+    end = time.time()
+
     print('GRNN: ')
-    print('Precisão: '+str(round(score,4)))
+    print('Precisão: ' + str(round(score, 4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
 
     classifier = Earth()
     classifier.fit(X_train, Y_train)
@@ -265,7 +380,21 @@ def classificaPCA(X_train,X_test,Y_train,Y_test,components):
     Y_Hat = np.around(Y_Hat)
     score = accuracy_score(Y_test, Y_Hat)
 
-    print('MARS: ')
-    print('Precisão: '+str(round(score,4)))
     end = time.time()
+
+    print('MARS: ')
+    print('Precisão: ' + str(round(score, 4)))
+    print('Tempo de execução:' + str(end - start))
+
+    start = time.time()
+
+    classifier = DBSCAN()
+    classifier.fit(X_train)
+    Y_Hat = classifier.fit_predict(X_test)
+    score = accuracy_score(Y_test, Y_Hat)
+
+    end = time.time()
+
+    print('DBSCAN: ')
+    print('Precisão: ' + str(round(score, 4)))
     print('Tempo de execução:' + str(end - start))
--- util.py
+++ util.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-#Define os indices de substituição numéricos com base na média 
-#do parametro referencia para cada um, facilitando a identificação
-#da correlação entre o indice e a referência.
-def getOrderedRemapDict(data,target,reference):
+# Define os indices de substituição numéricos com base na média
+# do parametro referencia para cada um, facilitando a identificação
+# da correlação entre o indice e a referência.
+def remap_dict(data, target, reference):
     ordered_data = data.groupby(target)[reference].mean().sort_values(ascending=True)
     dictRemap={}
     index=0
@@ -12,31 +12,31 @@ def getOrderedRemapDict(data,target,reference):
         index = index+1
     return dictRemap
 
-#Normaliza os dados para média zero e desvio padrão 1
-def normalizeData(data):
+# Normaliza os dados para média zero e desvio padrão 1
+def normalize_data(data):
     for column in data:
         data[column]=(data[column]-data[column].mean())/data[column].std()
     return data
 
-#Limpa os dados e aplica os indices calculados com getOrderedRemapDict()
-def castToNumeric(data):
+# Limpa os dados e aplica os indices calculados com getOrderedRemapDict()
+def cast_to_numeric(data):
     data['occupation'] = data['occupation'].str.strip()
     data['income'] = data['income'].str.strip()
     data['income'] = data['income'].map({'<=50K': 0, '>50K': 1}).astype(int)
     data['sex'] = data['sex'].str.strip()
     data['race'] = data['race'].str.strip()
     data['marital-status'] = data['marital-status'].str.strip()
-    data['relationship'] = data['relationship'].map(getOrderedRemapDict(data,'relationship','income')).astype(int)
-    data['native-country'] = data['native-country'].map(getOrderedRemapDict(data,'native-country','income')).astype(int)
-    data['workclass'] = data['workclass'].map(getOrderedRemapDict(data,'workclass','income')).astype(int)
-    data['occupation'] = data['occupation'].map(getOrderedRemapDict(data,'occupation','income')).astype(int)
-    data['sex'] = data['sex'].map(getOrderedRemapDict(data,'sex','income')).astype(int)
-    data['race'] = data['race'].map(getOrderedRemapDict(data,'race','income')).astype(int)
-    data['marital-status'] = data['marital-status'].map(getOrderedRemapDict(data,'marital-status','income')).astype(int)
+    data['relationship'] = data['relationship'].map(remap_dict(data, 'relationship', 'income')).astype(int)
+    data['native-country'] = data['native-country'].map(remap_dict(data, 'native-country', 'income')).astype(int)
+    data['workclass'] = data['workclass'].map(remap_dict(data, 'workclass', 'income')).astype(int)
+    data['occupation'] = data['occupation'].map(remap_dict(data, 'occupation', 'income')).astype(int)
+    data['sex'] = data['sex'].map(remap_dict(data, 'sex', 'income')).astype(int)
+    data['race'] = data['race'].map(remap_dict(data, 'race', 'income')).astype(int)
+    data['marital-status'] = data['marital-status'].map(remap_dict(data, 'marital-status', 'income')).astype(int)
     return data
 
-#Corrige o skew das colunas de dados selecionadas usando a raiz cubica.
-def correctSkewness(data, columns):
+# Corrige o skew das colunas de dados selecionadas usando a raiz cubica.
+def correct_skewness(data, columns):
     for column in columns:
         data[column] = np.cbrt(data[column])
     return data
